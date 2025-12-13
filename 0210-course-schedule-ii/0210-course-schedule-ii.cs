2373ad68fbf0813d1001636d9333734cfd4200af
Youssef Gaber <128910426+Youssef11gaber10@users.noreply.github.com>
2025-12-13 23:30:23 +0200

    3 ms (93.72%) 52.6 MB (45.29%) 0210-course-schedule-ii Medium .cs

--- 0210-course-schedule-ii/0210-course-schedule-ii.cs
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cs
@@ -1,68 +1,61 @@
 public class Solution {
-        public int[] FindOrder(int numCourses, int[][] prerequisites)
+       public int[] FindOrder(int numCourses, int[][] prerequisites)
+ {
+     List<List<int>> adj = new List<List<int>>();
+     for (int i = 0; i < numCourses; i++)
+         adj.Add(new List<int>());
+
+     foreach (int[] row in prerequisites)
      {
-         List<List<int>> adj = new List<List<int>>();
-         for (int i = 0; i < numCourses; i++)
-             adj.Add(new List<int>());
-         foreach (int[] row in prerequisites)
-         {
-             adj[row[1]].Add(row[0]);//directed
-         }
-         //finished adjacecy list
+         adj[row[1]].Add(row[0]);//directed
+     }
+     //finish adj list
 
-         bool[] visited = new bool[numCourses];
-         bool[] curr_path = new bool[numCourses];
-         Stack<int> stack = new Stack<int>();
-         int[] topological_sort = new int[numCourses];
+     bool[] visited = new bool[numCourses];
+     bool[] currPath = new bool[numCourses];
+     Stack<int> stack = new Stack<int>();
+     int[] topological_sort = new int[numCourses];
 
-         for (int i = 0; i < numCourses; i++)//cause if the graph not connected visit all nodes
-         {
-             if (!visited[i])//if the post dfs ended and there remain dfs not visted => dfs them
-             {
-                 if (Dfs_postorder(adj, i, visited, curr_path, stack) == false)
-                     return new int[0];//this is have cycle can't have topological sort
-             }
-         }
-         //reverse stack
-         for (int i = 0; i < numCourses; i++)
-         {
-             if (stack.Count > 0)
-             {
-                 topological_sort[i] = stack.Pop();
-             }
-         }
-
-         return topological_sort;
-
-
-
-     }//end of func
-
-
-     bool Dfs_postorder(List<List<int>> adj, int node, bool[] visited, bool[] curr_path, Stack<int> stack)
+     for (int i = 0; i < numCourses; i++)
      {
-         //cause curr_path walk like dfs so i should keep curr path like dfs preorder and also make it visited
-
-         if (visited[node] && curr_path[node])//there cycle in this graph can't do topological sort retun false
-             return false;
-         if (visited[node])
-             return true;//okay don't have cycle continue dfs
-
-         curr_path[node] = true;
-         visited[node] = true;
-
-         //first visit neibour after finsih them put node(parent) in stack//this is pre order
-         foreach (int nei in adj[node])
-         {
-             if (Dfs_postorder(adj, nei, visited, curr_path, stack) == false) //from any neibour found cycle
-                 return false;
-         }
-         //second//after finsih neibour 1=> remove it from curr_path 2=> put node(parent) in stack 
-         curr_path[node] = false;
-         stack.Push(node);
-         return true;
-
+         if (Dfs(adj, i, stack, visited, currPath) == false)
+             return new int[0];
      }
 
+     for (int i = 0; i < numCourses; i++)
+     {
+         if (stack.Count > 0)
+         {
+             topological_sort[i] = stack.Pop();
+         }
+     }
+
+     return topological_sort;
+
+ }//endof func
+
+
+ bool Dfs(List<List<int>> adj, int node, Stack<int> stack, bool[] visited, bool[] curr_path)
+ {
+     if (curr_path[node])
+         return false;
+     if (visited[node])
+         return true;
+
+     curr_path[node] = true;
+     visited[node] = true;
+
+
+     foreach(int nei in adj[node])
+     {
+         if (Dfs(adj, nei, stack, visited, curr_path) == false)
+             return false;
+     }
+     curr_path[node] = false;
+     stack.Push(node);
+     return true;
+
+
+ }
 
 }
