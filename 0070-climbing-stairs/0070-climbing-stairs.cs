69974bad355209b2dde84ac3abdaabd247face76
Youssef Gaber <128910426+Youssef11gaber10@users.noreply.github.com>
2025-12-20 17:42:52 +0200

    0 ms (100%) 29.2 MB (33.95%) 0070-climbing-stairs Easy .cs

--- 0070-climbing-stairs/0070-climbing-stairs.cs
+++ 0070-climbing-stairs/0070-climbing-stairs.cs
@@ -1,17 +1,17 @@
 public class Solution {
 
-public int ClimbStairs(int n)
-{
+ public int ClimbStairs(int n)
+ {
+     List<int> list = new List<int>();
+     list.Add(1);//[0]
+     list.Add(1);//[1]
+     for (int i = 2; i <= n; i++)
+     {
+         list.Add(list[i - 1] + list[i-2]); //like list[2].add(list[i-1]+list[i+2])
+     }
 
-    int[] dp = new int[n+1];
-    dp[0] = 1;
-    dp[1] = 1;
-    for (int i = 2; i <= n; i++)
-    {
-        dp[i] = dp[i - 1] + dp[i - 2];
-    }
+     return list[n];
+ }
 
-    return dp[n];
-}
 
 }
