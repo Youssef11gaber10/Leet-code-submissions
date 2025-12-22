4731657b7b4c49065a952cf3ad121d318da549a1
Youssef Gaber <128910426+Youssef11gaber10@users.noreply.github.com>
2025-12-22 08:04:30 +0200

    5 ms (2.44%) 40.1 MB (81.18%) 0118-pascals-triangle Easy .cs

--- 0118-pascals-triangle/0118-pascals-triangle.cs
+++ 0118-pascals-triangle/0118-pascals-triangle.cs
@@ -1,46 +1,29 @@
 public class Solution {
-    public IList<IList<int>> Generate(int numRows) {
-        
+    Dictionary<(int,int), int> memo = new Dictionary<(int,int), int>();
+ int pascal_rec(int prev_row, int curr_row)
+ {
+     if (curr_row == 0 || curr_row == prev_row)//if you at first or at end put 1
+         return 1;
+     if (memo.ContainsKey((curr_row, prev_row)))//if i count before 1+2 and you record it value gie me sol
+        return memo[(curr_row, prev_row)];
 
+     int result_curr = pascal_rec(prev_row - 1, curr_row - 1) + pascal_rec(prev_row - 1, curr_row);
+     memo[(curr_row, prev_row)] = result_curr;
+     return result_curr;
+ }
+ public IList<IList<int>> Generate(int numRows)
+ {
+     IList<IList<int>> triangle = new List<IList<int>>();
+     for (int i = 0; i < numRows; i++)
+     {
+         List<int> row = new List<int>();
+         for (int j = 0; j <= i; j++)
+         {
+             row.Add(pascal_rec(i, j)); //if i=0,j=0 or j=i both return 1 else go inside rec
+         }
+         triangle.Add(row);
+     }
+     return triangle;
+ }
 
-
-            IList< IList<int>> pascal_triangle = new List<IList<int>>();
-            pascal_triangle.Add(new List<int>() { 1 });
-
-
-            for (int i = 1; i < numRows; i++)
-            {
-                //make list
-                IList<int> row = new List<int>();
-                IList<int> prev_row = new List<int>();
-
-                // add 1 in begin and end
-                row.Add(1); 
-
-                //get the prev row and add each two element and added after first 1 added
-                prev_row = pascal_triangle[i-1];
-
-                //for loop on prev row  
-                for (int j = 0; j < prev_row.Count-1; j++)
-                {
-                    row.Add(prev_row[j] + prev_row[j+1]);
-                }
-
-
-
-                
-                
-                //add 1 in begin and end
-                row.Add(1);
-                //add row in  trangle
-                pascal_triangle.Add(row);
-
-            }
-
-            return pascal_triangle;
-
-
-
-        
-    }
 }
