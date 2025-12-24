64efecf6fc687a2ad8763f93c72b0ddd2c5b33a8
Youssef Gaber <128910426+Youssef11gaber10@users.noreply.github.com>
2025-12-24 03:35:59 +0200

    4 ms (8.42%) 33.1 MB (5.26%) 1025-divisor-game Easy .cs

--- 1025-divisor-game/1025-divisor-game.cs
+++ 1025-divisor-game/1025-divisor-game.cs
@@ -1,8 +1,42 @@
 public class Solution {
-    public bool DivisorGame(int n) {
-        if(n%2 ==0 )
-        return true;
-        else 
-        return false;
+    Dictionary<int, bool> dp = new Dictionary<int, bool>();
+
+public bool DivisorGame(int n)
+{
+    dp[1] = false;
+    dp[2] = true;
+    return Rec(n);
+}
+
+bool Rec(int n)
+{
+    if (dp.ContainsKey(n))
+        return dp[n];
+
+    foreach (int x in GetDivisors(n))
+    {
+        if (!Rec(n - x))
+        {
+            dp[n] = true;
+            return true;
+        }
     }
+
+    dp[n] = false;
+    return false;
+}
+
+List<int> GetDivisors(int n)
+{
+    List<int> res = new List<int>();
+    for (int i = 1; i * i <= n; i++)
+    {
+        if (n % i == 0)
+        {
+            if (i < n) res.Add(i);
+            if (n / i != i && n / i < n) res.Add(n / i);
+        }
+    }
+    return res;
+}
 }
