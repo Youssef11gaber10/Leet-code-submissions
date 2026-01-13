c16f30eadb8019404c5d58d266cd6f31553d309f
Youssef Gaber <128910426+Youssef11gaber10@users.noreply.github.com>
2026-01-13 22:39:22 +0200

    13 ms (63.33%) 52.3 MB (48.93%) 0560-subarray-sum-equals-k Medium .cs

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cs
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cs
@@ -1,21 +1,35 @@
 public class Solution {
-       public int SubarraySum(int[] nums, int k)
+        public int SubarraySum(int[] nums, int k)
+  {
+      int[] prefix_arr = new int[nums.Length];
+      prefix_arr[0] = nums[0];
+      for (int i = 1; i < nums.Length; i++)
       {
-          int count = 0;
-          for (int i = 0; i < nums.Length; i++)
+          prefix_arr[i] = prefix_arr[i - 1] + nums[i];
+      }
+      //after finish this i have array of prefix sum
+      Dictionary<int, int> dict = new Dictionary<int, int>();
+      dict[0] = 1;
+      int count = 0;
+      for (int i = 0; i < nums.Length; i++)
+      {
+          int candidate = prefix_arr[i] - k;
+          if (dict.ContainsKey(candidate))
           {
-              int sum = 0;
-              for(int j = i; j < nums.Length;  j++)
-              {
-                  sum += nums[j];
-                  if( k == sum)
-                  {
-                      count++;
-                  }
-              }
+              count += dict[candidate];
           }
-          return count;
+          if (dict.ContainsKey(prefix_arr[i]))
+          {
+              dict[prefix_arr[i]]++;
+          }
+          else
+          {
+              dict[prefix_arr[i]] = 1;
+          }
 
       }
+      return count;
+
+  }
 
 }
