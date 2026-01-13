9177ff86c496023a89b2117fbc4e5f705971fb32
Youssef Gaber <128910426+Youssef11gaber10@users.noreply.github.com>
2026-01-13 22:02:09 +0200

    1219 ms (11.2%) 52.4 MB (48.93%) 0560-subarray-sum-equals-k Medium .cs

---
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cs
@@ -0,0 +1,21 @@
+public class Solution {
+       public int SubarraySum(int[] nums, int k)
+      {
+          int count = 0;
+          for (int i = 0; i < nums.Length; i++)
+          {
+              int sum = 0;
+              for(int j = i; j < nums.Length;  j++)
+              {
+                  sum += nums[j];
+                  if( k == sum)
+                  {
+                      count++;
+                  }
+              }
+          }
+          return count;
+
+      }
+
+}
