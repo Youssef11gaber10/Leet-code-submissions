c6f66ea65f6adc3d0f2f09e9bc9a7f50ffc3041a
Youssef Gaber <128910426+Youssef11gaber10@users.noreply.github.com>
2026-01-14 23:47:32 +0200

    0 ms (100%) 44.8 MB (87.31%) 3745-maximize-expression-of-three-elements Easy .cs

--- 3745-maximize-expression-of-three-elements/3745-maximize-expression-of-three-elements.cs
+++ 3745-maximize-expression-of-three-elements/3745-maximize-expression-of-three-elements.cs
@@ -1,8 +1,27 @@
 public class Solution {
-    public int MaximizeExpressionOfThree(int[] nums)
+  public int MaximizeExpressionOfThree(int[] nums)
  {
+     int max1 = int.MinValue;
+     int max2 = int.MinValue;
+     int min = int.MaxValue;
+     foreach ( int n in nums )
+     {
+         if ( n > max1 )
+         {
+             max2 = max1;
+             max1 = n;
+         }
+         else if(n> max2)
+         {
+             max2 = n;
+         }
 
-     Array.Sort(nums);
-     return nums[nums.Length-1]+ nums[nums.Length - 2] - nums[0];
+        if(n < min) 
+         {
+             min = n;
+         }
+     }
+     return max1+max2-min;
+
  }
 }
