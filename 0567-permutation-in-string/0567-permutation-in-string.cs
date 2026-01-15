d84688c174e26e3c2f690e9d8c3264c985fe64fc
Youssef Gaber <128910426+Youssef11gaber10@users.noreply.github.com>
2026-01-15 22:40:35 +0200

    692 ms (5.89%) 58.4 MB (8.84%) 0567-permutation-in-string Medium .cs

--- 0567-permutation-in-string/0567-permutation-in-string.cs
+++ 0567-permutation-in-string/0567-permutation-in-string.cs
@@ -1,66 +1,54 @@
 public class Solution {
-      public bool CheckInclusion(string s1, string s2)
-  {
-      Dictionary<char,int> map1= new Dictionary<char,int>();
-      Dictionary<char,int> map2= new Dictionary<char,int>();
 
-      foreach (char s in  s1)
-      {
-          if (map1.ContainsKey(s))
-              map1[s]++;
-          else
-              map1[s] = 1;
-      }
-      //start fixed sliding window
-      int start = 0, end = s1.Length - 1;//its fixed to s1.length
-      while (end < s2.Length)
-      {
-          if (!(map1.ContainsKey(s2[start]) && map1.ContainsKey(s2[end])))
-          {
-              start++;
-              end++;
-          }
-          else
-          {
-              int it = start;
-              while (it <= end)
-              {
-                  if (map2.ContainsKey(s2[it]))
-                      map2[s2[it]]++;
-                  else
-                      map2[s2[it]] = 1;
-                  it++;
-              }
-              //now check if map1 ,map2 are equals
-              bool equals = true;
-              foreach(var pair in map2)
-              {
-                  if (!map1.ContainsKey(pair.Key))
-                  {
-                      equals = false;
-                      break;
-                  }
-                  if ( map1[pair.Key] != pair.Value)
-                  {
-                      equals = false;
-                      break;
-                  }
+    public bool CheckInclusion(string s1, string s2)
+    {
+        if (s1.Length > s2.Length)
+            return false;
 
+        Dictionary<char, int> map1 = new Dictionary<char, int>();
+        
+        foreach (char c in s1)
+        {
+            if (map1.ContainsKey(c))
+                map1[c]++;
+            else
+                map1[c] = 1;
+        }
+        
+        int windowSize = s1.Length;
+        
+        for (int i = 0; i <= s2.Length - windowSize; i++)
+        {
+            Dictionary<char, int> map2 = new Dictionary<char, int>();
+            
+            for (int j = i; j < i + windowSize; j++)
+            {
+                char c = s2[j];
+                if (map2.ContainsKey(c))
+                    map2[c]++;
+                else
+                    map2[c] = 1;
+            }
+            
+            if (AreDictionariesEqual(map1, map2))
+                return true;
+        }
+        
+        return false;
+    }
+    
+    private bool AreDictionariesEqual(Dictionary<char, int> map1, Dictionary<char, int> map2)
+    {
+        if (map1.Count != map2.Count)
+            return false;
+        
+        foreach (var pair in map1)
+        {
+            if (!map2.TryGetValue(pair.Key, out int value) || value != pair.Value)
+                return false;
+        }
+        
+        return true;
+    }
 
-              }
-              if (equals == true)
-                  return true;
-              else
-              {
-                  map2.Clear();
-                  start++;
-                  end++;
-              }
-          }
-      }
-      return false;
-
-
-
-  }
 }
